8a599c6abaaf0418ede142b4f85d42bb02c390bc
oisdk <oisin.kidney@gmail.com>
2015-11-13 01:04:40 +0000

    parsing

--- mod.py
+++ mod.py
@@ -2,4 +2,4 @@ from subprocess import check_call
 check_call(["omc", "script.mos"])
 with open("PipesValveDiscrete_res.csv") as file:
     table = [[float(n) for n in row.split(",")] for row in file.read().split("\n")[1:-1]]
-    print(table)
+    for row in table: print(row)
