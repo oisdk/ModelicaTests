918b2ed73d5c9faeb8d5f8f31b350c0af5519a4a
oisdk <oisin.kidney@gmail.com>
2015-11-15 21:35:29 +0000

    added incomp

--- mod.py
+++ mod.py
@@ -1,5 +1,7 @@
+from pickle import dump
 from subprocess import check_call
 check_call(["omc", "script.mos"])
-with open("PipesValveDiscrete_res.csv") as file:
+with open("PipesValveIncompressible_res.csv") as file:
     table = [[float(n) for n in row.split(",")] for row in file.read().split("\n")[1:-1]]
-    for row in table: print(row)
+    with open('myfile.dat', 'w+') as file:
+        dump(table,file,2)
