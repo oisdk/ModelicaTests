2b86dffbcccb8ee739619b3b4965161996fb06d0
oisdk <oisin.kidney@gmail.com>
2015-11-16 17:49:44 +0000

    python glue

--- mod.py
+++
@@ -1,7 +0,0 @@
-from pickle import dump
-from subprocess import check_call
-check_call(["omc", "script.mos"])
-with open("PipesValveIncompressible_res.csv") as file:
-    table = [[float(n) for n in row.split(",")] for row in file.read().split("\n")[1:-1]]
-    with open('myfile.dat', 'w+') as file:
-        dump(table,file,2)
